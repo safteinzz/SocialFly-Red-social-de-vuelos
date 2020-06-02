5de0bcd311eab8ffb321b1d6580b1cb648453668
Pablo Hernandez <pablohernandezcontacto@gmail.com>
2020-06-02 19:00:42 +0200

    getkeyrelacion admin y cambios perfil.html

--- Firebase/public/resources/JS/admin.js
+++ Firebase/public/resources/JS/admin.js
@@ -1695,6 +1695,26 @@ $('.datepicker').datepicker({
 /**============================================================
                            BBDD
 ============================================================*/
+// <!------------------- Get key relacion ---------------------->
+function getKeyRelacion(table_name, campo_bd, valor_bd, campo2_bd, valor2_bd)						
+{
+	try {
+		var query = dbRef.child("/" + table_name + "/")
+			.orderByChild(campo_bd)
+			.equalTo(valor_bd);
+
+		var snap = await query.once("value");	
+		
+		if (snap.val() != null)
+		{
+			return snap.getKey();
+		}								
+	}
+	catch (error) {
+		alert("Se ha producido un error de en la gestión de " + table_name);
+		console.error(error);
+	}
+}
 // <!------------------- check relacion ---------------------->
 async function bbdd_existe_relacion(table_name, campo_bd, valor_bd, campo2_bd, valor2_bd) {
 	try {
